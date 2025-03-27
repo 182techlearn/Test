82b89f4f753b8883b9cf2eea36356221f4c01440
182techlearn <99007649+182techlearn@users.noreply.github.com>
2025-03-27 22:33:17 +0530

    Update Provider.tf

--- Provider.tf
+++ Provider.tf
@@ -1,3 +1,16 @@
 provider "aws"{
   region = "ap-south-1"
 }
+data "aws_ami" "latest_ami" {
+  most_recent = true
+  owners      = ["amazon"] # Replace with the appropriate owner ID
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"] # Replace with your AMI name pattern
+  }
+}
+resource "aws_instance" "myec2test" {
+  ami           = data.aws_ami.latest_ami.id
+  instance_type = var.instance_type[0]
+  key_name = "terraform-key"
+}
